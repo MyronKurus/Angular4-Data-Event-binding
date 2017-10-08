bf79282ce7e49d55d8e9612395dcfa0c03a07f06
Myron Kurus <MyronKurus@users.noreply.github.com>
2017-10-08 23:06:56 +0300

    data/event-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   serverElements = [{type: 'server', name: 'Test Server', content: 'Just a test'}];
 
+  ngOnInit() {}
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 
 }
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,27 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+
+  @Output() serverAdded = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintAdded = new EventEmitter<{serverName: string, serverContent: string}>();
+
   newServerName = '';
   newServerContent = '';
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverAdded.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
   onAddBlueprint() {
-  //   this.serverElements.push({
-  //     type: 'blueprint',
-  //     name: this.newServerName,
-  //     content: this.newServerContent
-  //   });
+    this.blueprintAdded.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
 }
